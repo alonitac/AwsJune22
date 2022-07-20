07029011b6526b96194142dfb26782c21a58a7e7
alonit <alon.itach@mail.huji.ac.il>
2022-07-20 08:18:39 +0300

    cloudwatch queue backlog metric

--- youtubeBot/index.js
+++ youtubeBot/index.js
@@ -3,11 +3,8 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const fs = require('fs');
-const ytdl = require('ytdl-core');
 const ytsr = require('ytsr');
 const AWS = require('aws-sdk');
-var ffmpeg = require('fluent-ffmpeg');
 var morgan = require('morgan');
 require('dotenv').config();
 
@@ -25,8 +22,9 @@ app.get('/', (req, res) =>  {
   res.sendFile(__dirname + '/');
 });
 
-const sqs = new AWS.SQS({region: 'eu-north-1'});
-
+const sqs = new AWS.SQS({region: process.env.SQS_REGION});
+const cw = new AWS.CloudWatch({region: process.env.SQS_REGION});
+const autoscaling = new AWS.AutoScaling({region: process.env.SQS_REGION});
 
 app.get('/status', (req, res) => {
 	res.sendStatus(200);
@@ -79,5 +77,50 @@ app.post('/youtube', (req, res) =>  {
 		res.json({'text': 'Something went wrong... please try again'});
 	});
 });
+
+async function calcMsgsInQueue() {
+        const params = {
+          QueueUrl: process.env.QUEUE_URL,
+          AttributeNames: ["ApproximateNumberOfMessages"]
+        };
+        sqs.getQueueAttributes(params, function(err, data) {
+          if (err) {
+            console.log(err, err.stack); // an error occurred
+          } else {
+               const msgsInQueue = data.Attributes.ApproximateNumberOfMessages;
+               autoscaling.describeAutoScalingGroups({
+                    AutoScalingGroupNames: [process.env.ASG_NAME]
+               }, function(err, data) {
+                   if (err){ console.log(err, err.stack); } // an error occurred
+                   else {
+                        const asgSize = data.AutoScalingGroups[0].DesiredCapacity
+                        console.log(`autoscaling group size ${asgSize}`);
+                        console.log(`Messages in queue: ${msgsInQueue}`);
+                        const backlogPerInstance = asgSize === 0 ? 99 : parseInt(msgsInQueue) / asgSize
+                        const params = {
+                          MetricData: [
+                            {
+                              MetricName: process.env.QUEUE_URL.split('/').pop() + '-backlog-per-instance',
+                              Unit: 'None',
+                              Value: backlogPerInstance
+                            }
+                          ],
+                          Namespace: 'YoutubeBot'
+                        };
+
+                        cw.putMetricData(params, function(err, data) {
+                          if (err) {
+                            console.log("Error", err);
+                          } else {
+                            console.log("Success", JSON.stringify(data));
+                          }
+                        });
+                   }
+               });
+          }
+        });
+}
+
+setInterval(calcMsgsInQueue, 60000);
 
 http.listen(3000, () => console.log('listening on *:3000'));
